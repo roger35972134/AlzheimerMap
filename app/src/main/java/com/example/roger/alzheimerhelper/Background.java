7a2c7cd2bea5186d772dbd47d06525f7b45b011a
Roger Wang <roger35972134@gmail.com>
2016-01-18 19:11:27 +0800

    2016/1/18

---
+++ app/src/main/java/com/example/roger/alzheimerhelper/Background.java
@@ -0,0 +1,44 @@
+package com.example.roger.alzheimerhelper;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+public class Background extends Service {
+    String Alzheimer = "ALZHEIMER";
+    String x, y;
+    MyReceiver myReceiver;
+
+    public int onStartCommand(Intent intent, int flag, int startId) {
+        super.onStartCommand(intent, flag, startId);
+        myReceiver = new MyReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Alzheimer);
+        this.registerReceiver(myReceiver, filter);
+
+        return START_STICKY;
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) { return null; }
+
+    public class MyReceiver extends BroadcastReceiver {
+        public void onReceive(Context context, Intent intent) {
+            Bundle bundle = intent.getExtras();
+            String a = bundle.getString("POSITION");
+            Toast.makeText(Background.this, a, Toast.LENGTH_LONG).show();//intent.getStringExtra("POSITION") , Toast.LENGTH_LONG).show();
+            System.out.println(a);
+        }
+    }
+}
